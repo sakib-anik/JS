226b95ad90a2534f88ed4897b6722c5d07ac8d7c
MD. NAZMUS SAKIB <sakib.within@gmail.com>
2023-06-19 07:05:29 -0700

    Add files via upload

---
+++ Special/Callback&HigherOrderFunction/index.js
@@ -0,0 +1,35 @@
+const taskOne = (callback) => {
+    console.log('Task1');
+    callback();
+}
+
+const taskTwo = (callback) => {
+    setTimeout(()=>{
+        console.log('Task2. Data Loading');
+        callback();
+    },3000);
+}
+
+const taskThree = (callback) => {
+    console.log('Task3');
+    callback();
+}
+
+const taskFour = (callback) => {
+    console.log('Task4');
+    callback();
+}
+
+const taskFive = () => {
+    console.log('Task5');
+}
+
+taskOne(()=>{
+    taskTwo(()=>{
+        taskThree(()=>{
+            taskFour(()=>{
+                taskFive();
+            })
+        });
+    });
+});
